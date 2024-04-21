ee449c9d723a39e45cd6348954cea1242285e275
Thurisazz <zyukan1@gmail.com>
2024-04-21 21:58:59 +0300

    datetime.now() ---> datetime.today().date()

--- get_days_from _today.py
+++ get_days_from _today.py
@@ -3,11 +3,12 @@ from datetime import datetime, timedelta
 
 def get_days_from_today(date):
     try:
-        current_datetime = datetime.now()
-        datetime_objects = datetime.strptime(date, "%Y-%m-%d")
-        difference = current_datetime - datetime_objects
-        print(f"Difference in days: {abs(difference.days)}")
-        return abs(difference.days)
+        current_datetime = datetime.today().date()
+        print(current_datetime)
+        datetime_objects = datetime.strptime(date, "%Y-%m-%d").date()
+        difference = (current_datetime - datetime_objects).days
+        print(f"Difference in days: {abs(difference)}")
+        return abs(difference)
     except ValueError:
         print("Incorrect date format")
         return None
